b715859fdfb95533f56a4688f2a591e0692bf811
Sebastien Bovo <sebastienbovo@outlook.com>
2017-06-15 13:24:50 +0200

    Modifies cookie creation

--- EnterpriseMode/Cookie.js
+++ EnterpriseMode/Cookie.js
@@ -1,10 +1,12 @@
 function CreateCookie()
 {
-    document.cookie = "username=SSO_sbovo; expires=Thu, 29 Dec 2022 12:00:00 UTC; path=/"; 
+    document.cookie = "SSO_cookie created at " + (new Date()).toUTCString() +
+     "; expires=Thu, 29 Dec 2022 12:00:00 UTC;"; 
 }
 
 function ReadCookie()
 {
     var x = document.cookie;
     document.getElementById("cookieText").innerHTML = x;
+    alert(x);
 }
