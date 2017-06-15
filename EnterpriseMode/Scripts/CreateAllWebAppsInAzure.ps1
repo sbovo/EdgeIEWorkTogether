f05a72885dc087e2cb0f87157161acb8e9a866d7
Sebastien Bovo <sebastienbovo@outlook.com>
2017-06-15 18:20:35 +0200

    Azure pages

--- EnterpriseMode/Scripts/CreateAllWebAppsInAzure.ps1
+++ EnterpriseMode/Scripts/CreateAllWebAppsInAzure.ps1
@@ -3,12 +3,13 @@ $AppServicePlan = "EdgeIEWorkTogether"
 $myResourceGroup = "EdgeIEWorkTogether"
 
 $webappnames = Get-Content AzureLinks.txt 
-
-
 foreach ($webappname in $webappnames)
 {
      New-AzureRmWebApp -Name $webappname -Location $location -AppServicePlan $AppServicePlan -ResourceGroupName $myResourceGroup
 }
+# The deployments are not scripted. The strict minimul is to deploy:
+# - indexAzure.html as index.html
+# - Cookie.js
 
 
 
